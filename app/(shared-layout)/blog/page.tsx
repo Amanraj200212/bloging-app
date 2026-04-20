e06a1dc3d12b586eb15c1ac0f658f264ddf61ba2
Aman <skylerop9999@email.com>
2026-04-20 18:06:06 +0530

    fix

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -7,7 +7,6 @@ import { Metadata } from "next";
 import { cacheLife, cacheTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
-import { connection } from "next/server";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
@@ -41,12 +40,9 @@ export default  function BlogPage(){
 async function LoadBlogList(){
 
   //make blog page cacahe
-  // "use cache";
-  // cacheLife("hours");
-  // cacheTag("blog");
-  await connection();
-  
-
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blog");
   const dataOfPosts = await fetchQuery(api.posts.getPosts);
 
   return (
